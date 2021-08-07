1a3ed497648e26192d4233351aea65bbc0d77ccc
Shoful <85850433+Shoful@users.noreply.github.com>
2021-08-06 21:32:41 -0400

    Add files via upload

---
+++ Module_5_Programming1/Challenge_3.js
@@ -0,0 +1,11 @@
+//https://codepen.io/shoful/pen/vYmVzYO
+str ="Wipe the sweat off my dome, spit the phlegm on the streets Suede Timbs on my feets makes my cipher complete whether cruising in a Sikh's cab, or Montero Jeep I can't call it, the beats make me falling asleep i keep falling, but never falling six feet deep I'm out for presidents to represent me, I'm out for presidents to represent me, I'm out for dead presidents to represent me, ";
+let arr = str.split("");
+for(let i=0; i<arr.length; i++){
+    if(arr[i]=="s" || arr[i]=="S"){
+        arr[i]="$";
+    }
+}
+
+str=arr.join(' ');
+console.log(str);
